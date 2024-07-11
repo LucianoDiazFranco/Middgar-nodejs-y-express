0ba1d8ac6d7fd4658599f00a5a86e3304543048e
LucianoDiazFranco <luciano.diaz.franco@gmail.com>
2024-07-11 14:02:43 -0300

    Añadir configuración de finales de línea en .gitattributes

---
+++ src/routes/personas.routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import pool from '../database.js'
+
+const router = Router();
+
+router.get('/list', async(req, res)=>{
+    try{
+        const [result] = await pool.query('SELECT * FROM personas');
+        res.render('personas/list', { personas: result });
+    }
+    catch(err){
+        res.status(500).json({message:err.message});
+    }
+});
+
+export default router;
