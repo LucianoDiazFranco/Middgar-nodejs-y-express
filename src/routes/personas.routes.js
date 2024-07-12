0610f2fa70977957695c8c56616e3ed7b94d7cda
LucianoDiazFranco <luciano.diaz.franco@gmail.com>
2024-07-12 10:20:10 -0300

    Crud Completo (eliminar,actualizar,listar,crear)

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -31,4 +31,42 @@ router.get('/list', async(req, res)=>{
     }
 });
 
+router.get('/edit/:id', async(req, res)=>{
+    try{
+        const {id} = req.params;
+        const [persona] = await pool.query('SELECT * FROM personas WHERE id = ?' , [id]);
+        const personaEdit = persona[0];
+        res.render('personas/edit', {persona: personaEdit});
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({message:err.message});
+    }
+} )
+
+router.post('/edit/:id', async(req, res)=>{
+    try{
+        const {name, lastname, age} = req.body;
+        const {id} = req.params;
+        const editPersona = {name, lastname, age};
+        await pool.query('UPDATE PERSONAS SET ? WHERE id = ?' , [editPersona,id]);
+        res.redirect('/list');
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({message:err.message});
+    }
+})
+
+router.get('/delete/:id',  async(req, res)=>{
+    try{
+        const {id} = req.params;
+        await pool.query('DELETE FROM personas WHERE id = ?', [id]);
+        res.redirect('/list');
+    }
+    catch(err){
+        res.status(500).json({message:err.message});
+    }
+})
+
 export default router;
