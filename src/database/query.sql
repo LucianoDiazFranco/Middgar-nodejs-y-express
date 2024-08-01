deaa3717251d84939aa2e015981e0d168605c72f
LucianoDiazFranco <luciano.diaz.franco@gmail.com>
2024-08-01 15:41:25 -0300

    base de datos creadaa

--- src/database/query.sql
+++ src/database/query.sql
@@ -55,9 +55,60 @@ CREATE TABLE pacto_familiar(
 );
 CREATE TABLE pagos(
     id_pagos INT AUTO_INCREMENT PRIMARY KEY,
-    dni INT 
+    dni INT,
+    fecha DATENOT NOT NULL,
+    monto FLOATNOT NOT NULL,
+    acreditado BOOLEAN NOT NULL,
+    comprobante BOOLEAN NOT NULL,
+    concepto VARCHAR(50) NOT NULL,
+    metodo_pago VARCHAR(50),
+    FOREIGN KEY (dni) REFERENCES persona(dni) 
 );
-
-
+CREATE TABLE actividad(
+    id_actividad INT AUTO_INCREMENT PRIMARY KEY,
+    id_educador INT,
+    fecha DATE,
+    lugar VARCHAR(50),
+    actividad_tipo VARCHAR(50),
+    FOREIGN KEY (id_educador) REFERENCES educador(id_educador) 
+); 
+CREATE TABLE administrador(
+    id_actividad INT,
+    id_educador INT,
+    PRIMARY KEY (id_actividad, id_educador),
+    FOREIGN KEY (id_educador) REFERENCES educador(id_educador),
+    FOREIGN KEY (id_actividad) REFERENCES actividad(id_actividad)
+); 
+CREATE TABLE registro_actividad(
+    id_registro INT AUTO_INCREMENT PRIMARY KEY,
+    id_actividad INT,
+    fecha DATE NOT NULL,
+    cantidad_participantes INT NOT NULL,
+    cantidad_educadores INT NOT NULL,
+    cantidad_colaboradores INT,
+    FOREIGN KEY (id_actividad) REFERENCES actividad(id_actividad),
+); 
+CREATE TABLE planilla_riesgo(
+    id_planilla INT AUTO_INCREMENT PRIMARY KEY,
+    id_actividad INT,
+    id_beneficiario INT,
+    fecha DATE NOT NULL,
+    lugar VARCHAR(50) NOT NULL,
+    diagnostico VARCHAR (100) NOT NULL,
+    adulto_responsable VARCHAR(50),
+    FOREIGN KEY (id_actividad) REFERENCES actividad(id_actividad),
+    FOREIGN KEY (id_beneficiario) REFERENCES beneficiario(id_beneficiario),
+);
+CREATE TABLE documentacion(
+    id_documentacion INT AUTO_INCREMENT PRIMARY KEY,
+    legajo INT FOREIGN KEY,
+    autorizacion_padre BOOLEAN,
+    vacunas BOOLEAN,
+    uso_de_imagen BOOLEAN,
+    autorizacion_salida BOOLEAN,
+    permiso_retiro BOOLEAN,
+    permiso_acampe BOOLEAN,
+    vencimiento DATETIME, 
+);
 
 SELECT * FROM personas;
