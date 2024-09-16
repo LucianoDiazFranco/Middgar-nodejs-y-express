afe5d0873422f3524d134963437fecb0cce824e6
LucianoDiazFranco <Luciano.diaz.franco@gmail.com>
2024-09-16 20:28:19 -0300

    primer paso para el registro

--- src/public/script/registro.js
+++ src/public/script/registro.js
@@ -1,4 +1,15 @@
-document.getElementById("registro-form").addEventListener("submit",(e)=>{
+document.getElementById("registro-form").addEventListener("submit",async(e)=>{
     e.preventDefault();
-    console.log(e)
+    console.log(e.target.children.user.value)
+    const res = await fetch("http://localhost:3000/api/registro",{
+        method:"POST",  
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            user: e.target.children.user.value,
+            email: e.target.children.email.value,
+            password: e.target.children.password.value
+        })
+    });
 })
