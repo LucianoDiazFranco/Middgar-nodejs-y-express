bec16bec543f1e520d13bb80d7fded53c38780a4
LucianoDiazFranco <luciano.diaz.franco@gmail.com>
2024-09-18 15:32:33 -0300

    redireccionamiento del registro ok

--- src/controllers/authentication.controller.js
+++ src/controllers/authentication.controller.js
@@ -1,28 +1,38 @@
+import bcryptjs from "bcryptjs";
+
+
 //no utilizamos bs pero lo realizamos localmente
 const usuarios = [{
-    user: "a",
-    email: "a@gs.com",
-    password: "a"
+    user: "gf",
+    email: "teste@exemplo.us",
+    password: "$2a$06$UhcqmNJj1DDJy4kyFwaxveUg0oAN.zXRQ.nkLk7bwXKemf.RSXwS."
 }]
-
-
 
 function login (req,res){
 
 }
 
-function registro (req,res){//validamos que haya una contraseña en el formulario
+async function registro (req,res){//validamos que haya una contraseña en el formulario
     console.log(req.body);
     const user = req.body.user;
     const password = req.body.password;
     const email = req.body.email;
     if(!user || !password || !email){
-        res.status(400).send({status:"Error",message:"Los campos estan imcompletos"})
+        return res.status(400).send({status:"Error",message:"Los campos estan imcompletos"})
     }
     const usuarioARevisar = usuarios.find(usuario => usuario.user ===user)
     if(usuarioARevisar){
-        res.status(400).send({status:"Error",message:"El usuario ya Existe"})
+        return res.status(400).send({status:"Error",message:"El usuario ya Existe"})
     }
+    const salt = await bcryptjs.genSalt(6); //proceso de encriptacion
+    const hashPassword = await bcryptjs.hash(password,salt); //incriptamos
+
+    const nuevoUsuario = {
+        user, email, password: hashPassword
+    }
+    usuarios.push(nuevoUsuario);
+    console.log(usuarios);
+    return res.status(201).send({status:"ok",message:`Usuario ${nuevoUsuario.user} agregado`,redirect:"/"});
 }
 
 export const methods = {
--- src/public/script/registro.js
+++ src/public/script/registro.js
@@ -1,3 +1,5 @@
+const mensajeError = document.getElementsByClassName("error")[0];
+
 document.getElementById("registro-form").addEventListener("submit",async(e)=>{
     e.preventDefault();
     console.log(e.target.children.user.value)
@@ -8,7 +10,13 @@ document.getElementById("registro-form").addEventListener("submit",async(e)=>{
         },
         body: JSON.stringify({
             user: e.target.children.user.value,
-            email: e.target.children.email.value
+            email: e.target.children.email.value,
+            password: e.target.children.password.value
         })
     })
+    if(!res.ok) return mensajeError.classList.toggle("escondido",false);
+    const resJson = await res.json();
+    if(resJson.redirect){
+        window.location.href = resJson.redirect;
+    }
 })
